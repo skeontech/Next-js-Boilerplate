e9a8cd7c4d81d14fe679407a0b1388ae43727488
kartik-skeontech <154083969+kartik-skeontech@users.noreply.github.com>
2025-01-18 12:58:27 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -22,7 +22,7 @@ export default withSentryConfig(
           // }
         }
     
-        config.resolve.fallback = { fs: require.resolve('fs-browserify'), path: require.resolve('path-browserify'), stream: require.resolve('stream-browserify'), crypto: require.resolve('crypto-browserify'), net: require.resolve('net-browserify'), tls: require.resolve('tls-browserify'), dns: require.resolve('dns-browserify'), worker_threads: false, tty: require.resolve('tty-browserify') };
+        config.resolve.fallback = { fs: false, path: require.resolve('path-browserify'), stream: require.resolve('stream-browserify'), crypto: require.resolve('crypto-browserify'), net: require.resolve('net-browserify'), tls: require.resolve('tls-browserify'), dns: require.resolve('dns-browserify'), worker_threads: false, tty: require.resolve('tty-browserify') };
     
         return config
       },

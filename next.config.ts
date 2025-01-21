0531c6017d11bd780c794628bb3cc8dec0633ade
Kartik Sorathiya <79984533+kartik-solvative@users.noreply.github.com>
2025-01-21 12:22:45 -0500

    fix(node_dependencies): fs-browserify

--- next.config.ts
+++ next.config.ts
@@ -22,7 +22,7 @@ export default withSentryConfig(
           // }
         }
 
-        config.resolve.fallback = { http: require.resolve('http-browserify'), fs: require.resolve('fs-browserify'), path: require.resolve('path-browserify'), stream: require.resolve('stream-browserify'), crypto: require.resolve('crypto-browserify'), net: require.resolve('net-browserify'), tls: require.resolve('tls-browserify'), dns: false, worker_threads: false, tty: require.resolve('tty-browserify') };
+        config.resolve.fallback = { http: require.resolve('http-browserify'), fs: false, path: require.resolve('path-browserify'), stream: require.resolve('stream-browserify'), crypto: require.resolve('crypto-browserify'), net: require.resolve('net-browserify'), tls: require.resolve('tls-browserify'), dns: false, worker_threads: false, tty: require.resolve('tty-browserify') };
 
         return config;
       },
